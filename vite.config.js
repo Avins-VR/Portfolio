b2f12f5797cd66f1393225b14fc737e855c7421b
Avins VR <avins2005@gmail.com>
2025-06-20 20:35:23 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio/', // 🔥 must match GitHub repo name exactly
+  base: '/Portfolio/', 
   plugins: [react()],
 });
